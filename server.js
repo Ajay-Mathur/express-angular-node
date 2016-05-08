b503cc1587c2dce674551f77514cb467c0484f00
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2016-05-08 11:52:57 +1000

    update comfig

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require('body-parser')
 var cors = require("cors");
-mongoose.connect("mongodb://localhost/blog");
+var db_name = "blog";
+mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
+//take advantage of openshift env vars when available:
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+}
+
+mongoose.connect(mongodb_connection_string);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 var schema = mongoose.Schema;
 var blogSchema = new schema({
 	'title' : String,
@@ -95,8 +104,6 @@ app.get("/pullStory",function(req,res){
 	})
 });
 
-
-
-
-app.listen(3002);
-console.log("listening at : 3002")
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
